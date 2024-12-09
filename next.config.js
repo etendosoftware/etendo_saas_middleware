1f087764c0ae0134599788f8a21197769812b597
sebastianbarrozo <sebastian.barrozo@etendo.software>
2024-12-09 11:52:14 -0300

    Feature ETP-759: Dockerfile improve

--- next.config.js
+++ next.config.js
@@ -3,8 +3,10 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
-  output: "standalone",
+  images: {
+    unoptimized: true
+  },
+  // output: "standalone", // Esta línea ha sido eliminada o comentada
 };
 
 module.exports = nextConfig;
